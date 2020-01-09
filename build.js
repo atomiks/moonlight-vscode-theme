20a4e7e2b3e6ffdce438fb18259e54ce2d11d6a2
atomiks <cc.glows@gmail.com>
2020-01-09 23:39:04 +1100

    chore: fix legacy theme build

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const colors = require('./src/colors');
 
 const files = ['moonlight-ii.json', 'moonlight-ii-italic.json'];
+const legacyFiles = ['moonlight.json', 'moonlight-italic.json'];
 
 files.forEach(file => {
   let text = fs.readFileSync(`./src/${file}`, 'utf8');
@@ -11,4 +12,11 @@ files.forEach(file => {
   });
 
   fs.writeFileSync(`./themes/${file}`, text);
+});
+
+legacyFiles.forEach(legacyFile => {
+  fs.writeFileSync(
+    `./themes/${legacyFile}`,
+    fs.readFileSync(`./src/${legacyFile}`, 'utf8')
+  );
 });
