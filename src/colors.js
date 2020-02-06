dffcda840668e7b61474160178a7c91a4d941916
atomiks <cc.glows@gmail.com>
2020-02-06 20:55:22 +1100

    fix: color tweaks

--- src/colors.js
+++ src/colors.js
@@ -1,6 +1,7 @@
 module.exports = {
   '--moonlight-desaturated-gray': '#7f85a3',
-  '--moonlight-dark-blue': '#82aaff',
+  '--moonlight-dark-blue': '#4976eb',
+  '--moonlight-blue': '#82aaff',
   '--moonlight-sky-blue': '#50c4fa',
   '--moonlight-cyan': '#86e1fc',
   '--moonlight-red': '#ff757f',
@@ -11,8 +12,8 @@ module.exports = {
   '--moonlight-dark-orange': '#fc7b7b',
   '--moonlight-teal': '#4fd6be',
   '--moonlight-green': '#c3e88d',
-  '--moonlight-purple': '#c597f9',
-  '--moonlight-pink': '#f7a4ec',
+  '--moonlight-purple': '#c099ff',
+  '--moonlight-pink': '#fca7ea',
   '--moonlight-indigo': '#7a88cf',
   '--moonlight-bright-cyan': '#b4f9f8',
 
@@ -20,7 +21,7 @@ module.exports = {
   '--moonlight-gray-9': '#b4c2f0',
   '--moonlight-gray-8': '#a9b8e8',
   '--moonlight-gray-7': '#828bb8',
-  '--moonlight-gray-6': '#4d5482',
+  '--moonlight-gray-6': '#444a73',
   '--moonlight-gray-5': '#2f334d',
   '--moonlight-gray-4': '#222436',
   '--moonlight-gray-3': '#1e2030',
