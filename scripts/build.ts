5f58b536940e58e02dbc572eb9cf0f9d4d200681
atomiks <cc.glows@gmail.com>
2024-01-13 18:56:11 +1100

    release: 0.11.0

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,14 @@
 import fs from 'node:fs';
 import { colors } from '../src/colors';
+import ui from '../src/ui.json';
 
 ['moonlight', 'moonlight-ii'].forEach((theme) => {
   let srcString = fs.readFileSync(`./src/${theme}.json`, 'utf-8');
+
+  const json = JSON.parse(srcString);
+  Object.assign(json, { colors: ui.colors });
+  srcString = JSON.stringify(json, null, 2);
+
   Object.entries(colors).forEach(([colorVar, hexStr]) => {
     srcString = srcString.replace(new RegExp(`\\{${colorVar}}`, 'g'), hexStr);
   });
